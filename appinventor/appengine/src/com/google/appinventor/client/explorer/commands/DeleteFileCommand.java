912ef51cbeebc26a65051fcef04c8b5e2a6de9f7
Chetan Patil <chetanpatil49554@gmail.com>
2020-03-14 01:41:51 +0530

    Added Type name to delete screen support

--- appinventor/appengine/src/com/google/appinventor/client/explorer/commands/DeleteFileCommand.java
+++ appinventor/appengine/src/com/google/appinventor/client/explorer/commands/DeleteFileCommand.java
@@ -6,15 +6,47 @@
 
 package com.google.appinventor.client.explorer.commands;
 
+import static com.google.appinventor.client.Ode.MESSAGES;
+
+import com.google.appinventor.client.DesignToolbar;
+import com.google.appinventor.client.editor.FileEditor;
+import com.google.appinventor.client.editor.ProjectEditor;
 import com.google.appinventor.client.Ode;
 import com.google.appinventor.client.OdeAsyncCallback;
 import com.google.appinventor.client.explorer.project.Project;
-import static com.google.appinventor.client.Ode.MESSAGES;
+//import static com.google.appinventor.client.Ode.MESSAGES;
+import com.google.appinventor.client.widgets.LabeledTextBox;
+import com.google.appinventor.client.youngandroid.TextValidators;
 import com.google.appinventor.shared.rpc.project.ProjectNode;
+import com.google.appinventor.shared.rpc.project.ProjectRootNode;
+import com.google.appinventor.shared.rpc.project.UserProject;
 import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidBlocksNode;
 import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidFormNode;
 import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidSourceNode;
 import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidYailNode;
+import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidPackageNode;
+import com.google.appinventor.shared.rpc.project.youngandroid.YoungAndroidProjectNode;
+import com.google.appinventor.client.tracking.Tracking;
+import com.google.appinventor.client.explorer.commands.SaveScreenCheckpointCommand;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.Command;
+
+import java.util.HashSet;
+import java.util.Set;
 import com.google.gwt.user.client.Window;
 
 /**
@@ -25,6 +57,7 @@ public class DeleteFileCommand extends ChainableCommand {
   /**
    * Creates a new command for deleting a file.
    */
+  private static final int MAX_FORM_COUNT = 10; 
   public DeleteFileCommand() {
     super(null); // no next command
   }
@@ -36,6 +69,195 @@ public class DeleteFileCommand extends ChainableCommand {
 
   @Override
   public void execute(final ProjectNode node) {
+	  if (node instanceof YoungAndroidSourceNode) {
+  		new NewFormDialog((YoungAndroidSourceNode) node).center();
+  		
+	  } 
+	  else {
+  		  executionFailedOrCanceled();
+  		  throw new IllegalArgumentException("node must be a YoungAndroidProjectNode");
+	  }
+  }
+
+  /**
+   * Shows a confirmation dialog.
+   *
+   * @return {@code true} if the delete file command should be executed or
+   *         {@code false} if it should be canceled
+   */
+  protected boolean deleteConfirmation() {
+    return Window.confirm(MESSAGES.reallyDeleteFile());
+  }
+  
+  private class NewFormDialog extends DialogBox {
+    // UI elements
+    private final LabeledTextBox newNameTextBox;
+	private final CheckBox cb = new CheckBox(MESSAGES.checkBoxButton());
+    private final Set<String> otherFormNames;
+
+    NewFormDialog(final YoungAndroidSourceNode projectRootNode) {
+      super(false, true);
+      setStylePrimaryName("ode-DialogBox");
+      VerticalPanel contentPanel = new VerticalPanel();
+	  setText(MESSAGES.removeFormButton());
+      final String prefix = "Screen";
+      final String abc;
+      final int prefixLength = prefix.length();
+      int highIndex = 0;
+      // Collect the existing form names so we can prevent duplicate form names.
+      otherFormNames = new HashSet<String>();
+
+      /*for (ProjectNode source : projectRootNode.getAllSourceNodes()) {
+        if (source instanceof YoungAndroidFormNode) {
+          String formName = ((YoungAndroidFormNode) source).getFormName();
+          otherFormNames.add(formName);
+
+          if (formName.startsWith(prefix)) {
+            try {
+              highIndex = Math.max(highIndex, Integer.parseInt(formName.substring(prefixLength)));
+            } catch (NumberFormatException e) {
+              continue;
+            }
+          }
+        }
+      }*/
+
+      String defaultFormName = prefix + (highIndex + 1);
+
+      newNameTextBox = new LabeledTextBox(MESSAGES.formNameLabel());
+      newNameTextBox.setText(defaultFormName);
+      newNameTextBox.getTextBox().addKeyUpHandler(new KeyUpHandler() {
+        @Override
+        public void onKeyUp(KeyUpEvent event) {
+          int keyCode = event.getNativeKeyCode();
+          if (keyCode == KeyCodes.KEY_ENTER) {
+            //handleOkClick(projectRootNode);
+          } else if (keyCode == KeyCodes.KEY_ESCAPE) {
+            hide();
+            //executionFailedOrCanceled();
+          }
+        }
+      });
+      contentPanel.add(newNameTextBox);
+
+      String cancelText = MESSAGES.cancelButton();
+      String okText = MESSAGES.okButton();
+
+      // Keeps track of the total number of screens.
+      int formCount = otherFormNames.size() + 1;
+      if (formCount > MAX_FORM_COUNT) {
+        HorizontalPanel errorPanel = new HorizontalPanel();
+        HTML tooManyScreensLabel = new HTML(MESSAGES.formCountErrorLabel());
+        errorPanel.add(tooManyScreensLabel);
+        errorPanel.setSize("100%", "24px");
+        contentPanel.add(errorPanel);
+
+        okText = MESSAGES.addScreenButton();
+        cancelText = MESSAGES.cancelScreenButton();
+
+        // okText = "Add";
+        // cancelText = "Don't Add";
+      }
+      Button cancelButton = new Button(cancelText);
+      cancelButton.addClickHandler(new ClickHandler() {
+        @Override
+        public void onClick(ClickEvent event) {
+          hide();
+          executionFailedOrCanceled();
+        }
+      });
+      
+	  cb.setValue(true);
+      final Button okButton = new Button(okText);
+      okButton.addClickHandler(new ClickHandler() {
+        @Override
+        public void onClick(ClickEvent event) {
+    	   handleOkClick(projectRootNode);    
+        }
+      });
+      
+      HorizontalPanel buttonPanel = new HorizontalPanel();
+      HorizontalPanel checkboxPanel = new HorizontalPanel();
+      buttonPanel.add(cancelButton);
+      buttonPanel.add(okButton);
+      checkboxPanel.add(cb);
+      checkboxPanel.setSize("100%", "20px");
+      contentPanel.add(checkboxPanel);
+      buttonPanel.setSize("100%", "24px");
+      contentPanel.add(buttonPanel);
+      contentPanel.setSize("320px", "100%");
+      okButton.setEnabled(false);
+      if (projectRootNode instanceof YoungAndroidFormNode) {
+          final String abc1 = ((YoungAndroidFormNode) projectRootNode).getFormName();
+          /*newNameTextBox.getTextBox().addKeyListener(new KeyAdapter() {
+    		public void keyReleased(KeyEvent e) { //watch for key strokes
+        	  if(newNameTextBox.getText().equals(abc1))
+                okButton.setEnabled(true);
+        	  else
+        	  {
+                okButton.setEnabled(false);
+        	  }
+    		}
+		  });*/
+		  
+		  newNameTextBox.getTextBox().addKeyUpHandler(new KeyUpHandler() {
+    		@Override
+    		public void onKeyUp(KeyUpEvent event) {
+      		  if(newNameTextBox.getText().equals(abc1))
+                okButton.setEnabled(true);
+        	  else
+        	  {
+                okButton.setEnabled(false);
+        	  }
+    		}
+  		  });
+   		     
+	  }
+
+      add(contentPanel);
+    }
+	
+    private void handleOkClick(YoungAndroidSourceNode projectRootNode) {
+      String newFormName = newNameTextBox.getText();
+      if (validate(newFormName)) {
+  		allowCheckPoint();
+        hide();
+        removeForm(projectRootNode);
+      } else {
+        newNameTextBox.setFocus(true);
+      }
+    }
+	
+    private boolean validate(String newFormName) {
+      // Check that it meets the formatting requirements.
+      if (!TextValidators.isValidIdentifier(newFormName)) {
+        Window.alert(MESSAGES.malformedFormNameError());
+        return false;
+      }
+
+      // Check for reserved words
+      if(TextValidators.isReservedName(newFormName)) {
+        Window.alert(MESSAGES.reservedNameError());
+        return false;
+      }
+
+      // Check that it's unique.
+      if (otherFormNames.contains(newFormName)) {
+        Window.alert(MESSAGES.duplicateFormNameError());
+        return false;
+      }
+
+      return true;
+    }
+
+    /**
+     * Adds a new form to the project.
+     *
+     * @param formName the new form name
+     
+    
+	*/
+	public void removeForm(final ProjectNode node) {
     if (deleteConfirmation()) {
       final Ode ode = Ode.getInstance();
 
@@ -106,14 +328,29 @@ public class DeleteFileCommand extends ChainableCommand {
       executionFailedOrCanceled();
     }
   }
+    @Override
+    public void show() {
+      super.show();
 
-  /**
-   * Shows a confirmation dialog.
-   *
-   * @return {@code true} if the delete file command should be executed or
-   *         {@code false} if it should be canceled
-   */
-  protected boolean deleteConfirmation() {
-    return Window.confirm(MESSAGES.reallyDeleteFile());
+      Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+        @Override
+        public void execute() {
+          newNameTextBox.setFocus(true);
+        }
+      });
+    }
+    public void allowCheckPoint(){
+      			if (cb.getValue() == true) {
+        		  ProjectRootNode projectRootNode1 = Ode.getInstance().getCurrentYoungAndroidProjectRootNode();
+  				  if (projectRootNode1 != null) {
+    				ChainableCommand cmd = new SaveAllEditorsCommand(
+        			new SaveScreenCheckpointCommand(true));
+    				cmd.startExecuteChain(Tracking.PROJECT_ACTION_CHECKPOINT_YA, projectRootNode1);
+  				  }
+    	  
+      			  
+      			
+      }
+    }
   }
 }
